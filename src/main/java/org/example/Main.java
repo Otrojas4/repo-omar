b957347487815a30fe01f9eb5a95154dac7ab18c
Otrojas4 <u201514329@upc.edu.pe>
2023-05-09 06:18:02 -0500

    Commit_tarea_javadoc
    Commit_tarea_javadoc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,21 @@
 package org.example;
 
+/**
+ * Esta clase principal del sistema
+ * permite definir uno metodo
+ * para imprimir un string
+ * @author User
+ *
+ */
 public class Main {
+	
+	
+	
+	
+    /**
+     * metodo que permite imprimir un string
+     * @param args - valor ingresado por el usuario
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
